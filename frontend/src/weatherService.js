1168feb25e4c3d4e781d7816d757ab45a6bedd83
Krishna Kumar Gupta <kumarkrishn663@gmail.com>
2023-07-17 14:18:12 +0530

    updated api links

--- frontend/src/weatherService.js
+++ frontend/src/weatherService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const makeIconURL = (iconId) => `https://openweathermap.org/img/wn/${iconId}@2x.png`;
 const getFormatWeatherData = async (city) => {
-    const {data} = await axios.get(`http://localhost:8000/?city=${city}`)
+    const {data} = await axios.get(`https://omnify.onrender.com/?city=${city}`)
 
     const {weather , main: {temp,feels_like, temp_min,temp_max, pressure,humidity}, wind:{speed}, sys:{country},name} = data.weather;
 
